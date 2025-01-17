937e54f2137c4bf13d3082528b3db5ce4913dbfe
pericolospalotes2 <todofoe@gmail.com>
2025-01-17 02:07:32 +0100

    2

--- index.php
+++ index.php
@@ -7,7 +7,16 @@ use TelegramBot\Api\BotApi;
 $telegram = new BotApi('7711572491:AAEzEFSI6VC4LQ2EetznAXAdG9-QoCzrkmk');
 
 
+$update = json_decode(file_get_contents('php://input'));
 
+if (isset($update->message->text)){
+
+$chatId = $update->message->chat->id;
+
+$text = $update->message->text;
+
+
+}
 
 
 ?>
