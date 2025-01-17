a62252d6b6d5e9ce1a6c717d3e3323e0604ec0e9
pericolospalotes2 <todofoe@gmail.com>
2025-01-17 18:03:12 +0100

    funcionando

--- index.php
+++ index.php
@@ -11,14 +11,16 @@ $telegram = new BotApi('7711572491:AAEzEFSI6VC4LQ2EetznAXAdG9-QoCzrkmk');
 $update = json_decode(file_get_contents('php://input'));
 
 if (isset($update->message->text)){
-
 $chatId = $update->message->chat->id;
-
 $text = $update->message->text;
 
 if($text==='Hola'){
     $telegram->sendMessage($chatId,"Hola como estas");
     
+}else{
+    $defaulMesasage="No entiendo este comando";
+    $telegram->sendMessage($chatId,$defaulMesasage);
+
 }
 
 }
